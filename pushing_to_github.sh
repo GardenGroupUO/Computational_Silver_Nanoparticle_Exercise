e4184d5b949d7c5fc55e09daf9b79bcbd08d5f5c
Geoffrey Weal <geoffreyweal@Geoffreys-MacBook-Pro.local>
2022-02-27 12:28:45 +1300

    Update the Computational Silver Nanoparticle Exercise. THIS INCLUDES A VERSION OF WORKSHEET 3.1 THAT ALLOWS CAPPING DURING A SIMULATION.

--- pushing_to_github.sh
+++ pushing_to_github.sh
@@ -1,5 +1,5 @@
 # upload to github
 git add .
-git commit -m 'Update the Computational Silver Nanoparticle Exercise.'
+git commit -m 'Update the Computational Silver Nanoparticle Exercise. THIS INCLUDES A VERSION OF WORKSHEET 3.1 THAT ALLOWS CAPPING DURING A SIMULATION.'
 # push your new commit:
 git push -u origin main
