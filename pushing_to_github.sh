00589dda3c465a41aad0c865c29f698e3c244bbe
GardenGroupUO <gardengroupuo@gmail.com>
2022-03-17 18:00:54 +1300

    Update the Computational Silver Nanoparticle Exercise.

--- pushing_to_github.sh
+++ pushing_to_github.sh
@@ -1,5 +1,7 @@
 # upload to github
+git config  user.name "GardenGroupUO"
+git config  user.email "gardengroupuo@gmail.com"
 git add .
 git commit -m 'Update the Computational Silver Nanoparticle Exercise.'
 # push your new commit:
-git push -u origin main --force
+git push -u origin main --force 
